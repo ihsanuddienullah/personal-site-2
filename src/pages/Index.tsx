56aeacda30213b9514fff89bb604fdfedc23f04b
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-06-27 10:46:49 +0700

    add pages

---
+++ src/pages/Index.tsx
@@ -0,0 +1,31 @@
+
+import { ThemeProvider } from '@/components/ThemeProvider';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import Skills from '@/components/Skills';
+import Experiences from '@/components/Experiences';
+import Projects from '@/components/Projects';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
+
+const Index = () => {
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="portfolio-theme">
+      <div className="min-h-screen">
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Experiences />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default Index;
